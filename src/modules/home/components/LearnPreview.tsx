bad08b2424c46713cb493da7eb8d660cde4d0ec0
Kevin <kevinadikasaputragithub@gmail.com>
2025-11-19 22:39:35 +0700

    :art: Style: render 3 page preview and free only for preview for home page

--- src/modules/home/components/LearnPreview.tsx
+++ src/modules/home/components/LearnPreview.tsx
@@ -1,4 +1,4 @@
-`use client;`
+`use client;`;
 
 import Link from 'next/link';
 import { BsArrowRightShort as ViewAllIcon } from 'react-icons/bs';
@@ -10,14 +10,14 @@ import LearnCarousel from './LearnCorousel';
 
 const LearnPreview = () => {
   return (
-    <section className="space-y-6">
-      <div className="flex items-center justify-between">
-        <SectionHeading title="Latest Learn" className="ml-1" />
+    <section className='space-y-6'>
+      <div className='flex items-center justify-between'>
+        <SectionHeading title='Latest Learn' className='ml-1' />
         <SectionSubHeading>
-          <Link href="/learn">
-            <div className="flex gap-1 hover:gap-3 transition-all duration-300 cursor-pointer text-sm text-neutral-700 dark:text-neutral-400 hover:text-neutral-700 hover:dark:text-neutral-300 mt-1">
-              <div className="flex">
-                View All <span className="hidden sm:block ml-1">Lessons</span>
+          <Link href='/learn'>
+            <div className='flex gap-1 hover:gap-3 transition-all duration-300 cursor-pointer text-sm text-neutral-700 dark:text-neutral-400 hover:text-neutral-700 hover:dark:text-neutral-300 mt-1'>
+              <div className='flex'>
+                View All <span className='hidden sm:block ml-1'>Lessons</span>
               </div>
               <ViewAllIcon size={22} />
             </div>
@@ -25,7 +25,7 @@ const LearnPreview = () => {
         </SectionSubHeading>
       </div>
 
-      <LearnCarousel perPage={4} showOnly="all" />
+      <LearnCarousel perPage={3} showOnly='free' />
     </section>
   );
 };
